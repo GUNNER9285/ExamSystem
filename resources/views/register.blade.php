86400e5fc09ec342fc095de538906022a668a0ae
GUNNER9285 <gunner9285@gmail.com>
2018-07-09 23:02:14 +0700

    make Form login&register

---
+++ resources/views/register.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="jumbotron">
+        <h3 class="display-4">Register</h3>
+        <form class="form-group">
+            <div class="form-group row">
+                <label for="username" class="col-sm-4 col-form-label text-md-right">{{ __('Username') }}</label>
+
+                <div class="col-md-6">
+                    <input id="username" type="text" class="form-control{{ $errors->has('username') ? ' is-invalid' : '' }}" name="username" value="{{ old('username') }}" required autofocus>
+
+                    @if ($errors->has('username'))
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $errors->first('username') }}</strong>
+                        </span>
+                    @endif
+                </div>
+            </div>
+
+            <div class="form-group row">
+                <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                <div class="col-md-6">
+                    <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+
+                    @if ($errors->has('password'))
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $errors->first('password') }}</strong>
+                        </span>
+                    @endif
+                </div>
+            </div>
+
+            <div class="form-group row mb-0">
+                <div class="col-md-8 offset-md-4">
+                    <button type="submit" class="btn btn-primary">
+                        {{ __('Submit') }}
+                    </button>
+                </div>
+            </div>
+        </form>
+        <div class="row">
+            <div class="col-md-8 offset-md-4">
+                <a href="{{URL::to('/')}}">{{'back'}}</a>
+            </div>
+        </div>
+    </div>
+@endsection
